fd2846d21a893fbb36293dd9d3d4a8d7bf34df52
Soham Saha <soham.saha74@gmail.com>
2018-06-13 00:42:09 -0700

    added circles that show up w/ images in them

--- FinalProject/plates/plate.js
+++ FinalProject/plates/plate.js
@@ -12,48 +12,28 @@ d3.csv("plate.csv", function(d, i, columns) {
     for (i = 1; i < columns.length; ++i){
         var str = d[columns[i]];
 
-        if(str.valueOf()==="Ptrue")
+        if(str.valueOf()==="Ptrue"){
             console.log("Got value: " + str);
-//        var circle = svg.append("circle")
-//                        .attr("class","node")
-//                        .style("fill","apple.png")
-//                        .style("stroke", "red")
-//                        .attr("cx", xAttr)
-//                        .attr("cy", 30)
-//                        .attr("r", 20);
-        
-//        var circle = svg.append("circle")
-//            .style("href","apple.png")
-//            .attr("cx", 400)
-//            .attr("cy", 400)
-//            .attr("r",20);
-        
         var circle = svg.append("circle")
-            .style("fill", "blue")
-            .attr("cx", 400)
-            .attr("cy", 400)
-            .attr("r",20);
+                        .style("fill","green")
+                        .style("stroke", "red")
+                        .attr("cx", xAttr)
+                        .attr("cy", 30)
+                        .attr("r", 20);
+        
+        var image = svg.append("svg:image")
+                        .attr("x",xAttr-10)
+                        .attr("y",20)
+                        .attr("height",20)
+                        .attr("width",20)
+                        .attr("xlink:href",d[columns[1]]);
         
         var text = svg.append("text")
                         .attr("x",xAttr)
                         .attr("y",200)
                         .text(function(d){return str});
         xAttr += 50;
-
-        
-        circle.append("image")
-                .attr("href","apple.png")
-                .attr("width","40px")
-                .attr("height","40px");
-        
-//        circle.append("image")
-//                .attr("xlink:href", function (d){
-//                    if (str.valueOf()==="Ptrue"){
-//                        return ("apple.png");
-//                    } else {
-//                        return ("apple.png");
-//                    }});
-        
+        }
     }
     },  function(error, data) {
         if (error) throw error;
